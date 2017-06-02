2183a0c5ea90e926044092baf696392b937f42d3
mingyili <1275656592@qq.com>
2017-06-02 15:16:17 +0800

    old

--- src/js/map.js
+++ src/js/map.js
@@ -2,13 +2,20 @@
     'use strict';
 
     var map;
-    
+
+    var iconBase = 'https://mingyili.github.io/assets/img/map/';
+    var markStyles = {
+        'location': {
+            icon: iconBase + 'location.png'
+        }
+    };
+
     // 初始化Map
     function initMap() {
-        
+
         // 中心定位为北京
         var center = {
-            lat: 39.908, 
+            lat: 39.908,
             lng: 116.395
         };
 
@@ -19,16 +26,18 @@
 
         initMark({
             location: { lat: 39.9087191, lng: 116.3952003},
-            title: '天安门'
+            title: '天安门',
+            type: 'location'
         });
     }
 
     // 初始化一个标记
     function initMark(data) {
         var marker = new google.maps.Marker({
-          position: data.location,
           map: map,
-          title: data.title
+          position: data.location,
+          title: data.title,
+          icon: markStyles[data.type].icon
         });
         /*marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
@@ -39,8 +48,8 @@
     var script = document.createElement('script');
     script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCzaIkBQ6lg2lNcpzXCe554Eusmdq9ujEE";
     script.onload = function() {
-        console.log('loaded');        
-        initMap()
+        console.log('loaded');
+        initMap();
     }
     script.onerror = function() {
         console.log('Google Map 加载失败');
